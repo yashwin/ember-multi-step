e479fb6336f21385ab9bb6760e6b10e5397f4dcc
kabirnbaum <kylewdgb69@gmail.com>
2017-07-26 23:08:31 -0500

    HANDLEBARS, EMBER: made left/right buttons disapear if no content to the left/right

--- addon/components/step-nav.js
+++ addon/components/step-nav.js
@@ -3,11 +3,18 @@ import layout from '../templates/components/step-nav';
 
 const {
   Component,
-  get
+  get,
+  computed
 } = Ember;
 
 export default Component.extend({
   classNames: ['ems-nav'],
+  notBegining: computed('step', function() {
+    return get(this, 'step') != 1;
+  }),
+  notEnd: computed('step', function() {
+    return get(this, 'step') != $('.ems-step').length;
+  }),
   layout,
   actions: {
     nextStep() {
