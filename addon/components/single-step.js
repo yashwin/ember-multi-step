a04fa2c5d500d880f1a0fc28e29d9a8c0532ec9d
kabirnbaum <kylewdgb69@gmail.com>
2017-07-29 16:11:42 -0500

    ESLINT: made eslint changes

--- addon/components/single-step.js
+++ addon/components/single-step.js
@@ -17,9 +17,9 @@ export default Component.extend({
     return `ems-${getWithDefault(this, 'num', 'hidden')}`;
   }),
   showStep: computed('num', 'step', function() {
-    if(getWithDefault(this, 'step', 1) === get(this, 'num')) {
+    if (getWithDefault(this, 'step', 1) === get(this, 'num')) {
       if (get(this, 'stepKey')) {
-        get(this,'canAdvance').validate(get(this, 'stepKey'));
+        get(this, 'canAdvance').validate(get(this, 'stepKey'));
       }
       return true;
     }
--- addon/components/step-nav.js
+++ addon/components/step-nav.js
@@ -7,18 +7,18 @@ const {
   computed
 } = Ember;
 
-const { empty } = computed
+const { empty } = computed;
 
 export default Component.extend({
   classNames: ['ems-nav'],
   layout,
+  moveNext: empty('canAdvance.errors'),
   notBegining: computed('step', function() {
     return get(this, 'step') != 1;
   }),
   notEnd: computed('step', function() {
     return get(this, 'step') != $('.ems-step').length;
   }),
-  moveNext: empty('canAdvance.errors'),
   actions: {
     nextStep() {
       get(this, 'changeStep')(get(this, 'step') + 1); // DDAU
