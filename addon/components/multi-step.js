9786f479f4b49ba12a75d22553d101aa7163c420
kabirnbaum <kylewdgb69@gmail.com>
2017-09-20 11:38:04 -0700

    FIX, EMBER: fixed default changestep function

--- addon/components/multi-step.js
+++ addon/components/multi-step.js
@@ -12,6 +12,20 @@ export default Component.extend({
   classNames: ['ems'],
   attributeBindings: ['step:data-step'],
   layout,
+  /**
+  * changes the current step to the specified step
+  * @param  {int} step number
+  */
+  changeStep(step){
+    let changeStep = get(this, 'changeStep');
+    if (changeStep && typeof changeStep === 'function') {
+      return changeStep(step);
+    }
+    // change the step if its greater than 0 and smaller than the last step
+    if (step > 0 && step < $('.ems-step').length + 1) {
+      set(this, 'step', step);
+    }
+  },
   init() {
     this._super(...arguments);
     if (!get(this, 'step')) {
@@ -19,19 +33,5 @@ export default Component.extend({
     }
   },
   actions: {
-    /**
-     * changes the current step to the specified step
-     * @param  {int} step number
-     */
-    changeStep(step) {
-      let changeStep = get(this, 'changeStep');
-      if (changeStep && typeof changeStep === 'function') {
-        return changeStep(step);
-      }
-      // change the step if its greater than 0 and smaller than the last step
-      if (step > 0 && step < $('.ems-step').length + 1) {
-        set(this, 'step', step);
-      }
-    }
   }
 });
